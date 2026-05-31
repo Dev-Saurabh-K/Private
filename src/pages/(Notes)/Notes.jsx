d0ed8220e4a3f67c003b636b17c9ca18cc0d05d4
Dev-Saurabh-K <saurabhkumar.sakr@gmail.com>
2026-05-31 14:24:09 +0530

    fix notes dynamic

--- src/pages/(Notes)/Notes.jsx
+++ src/pages/(Notes)/Notes.jsx
@@ -6,19 +6,19 @@ import {useTopicStore} from "../../store/topicStore";
 
 const Notes = () => {
 
-  const URL = `${import.meta.env.VITE_API_URL}/api/generate/notes`;
+  const topic_id = useTopicStore((state)=>state.topic_id);
+  const URL = `${import.meta.env.VITE_API_URL}/api/retrieve/notes?topic_id=${topic_id}`;
   const token = localStorage.getItem("access_token");
 
-  const topic_id = useTopicStore((state)=>state.topic_id);
+  // http://localhost:8000/api/retrieve/notes?topic_id=2
+
   const [notes, setNotes]=useState([]);
 
 
   useEffect(()=>{
     const fetchNotes = async()=>{
       console.log(topic_id)
-      const response = await axios.post(URL,{
-        topic_id:topic_id
-      },
+      const response = await axios.get(URL,
       {
         headers:{
           Authorization: `Bearer ${token}`,
